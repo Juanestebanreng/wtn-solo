fca8ebcf3b363422fd7fb90de13aa6190ad94f1c
Juanes <juanesteban.reng@gmail.com>
2026-06-26 12:20:10 +0100

    add PostHog analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Space_Grotesk, Inter, IBM_Plex_Mono } from 'next/font/google';
 import './globals.css';
+import { PHProvider } from './providers';
 
 const display = Space_Grotesk({
   subsets: ['latin'],
@@ -35,9 +36,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${display.variable} ${body.variable} ${mono.variable} font-body antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-}
+cd ~/Downloads/wtn-solo
+git add .
+git commit -m "add PostHog analytics"
+git push
